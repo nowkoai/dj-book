9e76f21746e68982eee131f7da7df3dd64766e65
nowkoai <hyamasit@gmail.com>
2021-10-10 22:06:59 +0900

    first commit

---
+++ mysite/wsgi.py
@@ -0,0 +1,8 @@
+import os
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
